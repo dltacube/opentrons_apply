c388a1c93ab653bbe1615fe06e6f0df7f40d0084
[heyoni] <jrevah@gmail.com>
2018-05-26 05:33:16 -0400

    final version.

--- main.py
+++ main.py
@@ -11,19 +11,18 @@ containers.create(
 )
 
 trash = containers.load('point', 'E2')
-plate = containers.load('96-flat', 'B1')
+plate = containers.load('96-PCR-tall', 'B1')
 samples = containers.load('96-flat', 'C1')
 mastermix = containers.load('tube-rack-2ml', 'A2')
 
 t10 = containers.load('tiprack-10ul', 'A1')
 t100 = containers.load('tiprack-20ul', 'A3')
 
-
 # pipettes
 p100 = instruments.Pipette(axis='b',
-                          max_volume=100,
-                          tip_racks=[t10],
-                          trash_container=trash)
+                           max_volume=100,
+                           tip_racks=[t10],
+                           trash_container=trash)
 p50 = instruments.Pipette(axis='a',
                           max_volume=50,
                           tip_racks=[t10],
@@ -34,6 +33,7 @@ p50 = instruments.Pipette(axis='a',
 def print_history():
     for c in robot.commands():
         print(c)
+
 
 def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11)), sybr_only=[0, 11]):
     # Step 1. Load the mastermix
@@ -47,36 +47,38 @@ def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11)), sybr_
 
     # run this loop once for each tip in the column, taking tips from A1...A12
     for tip in t100.cols(tip_start_col):
-        p100.pick_up_tip(tip)
-        # This loop runs once for every well in the current row of our destination plate, going from A1...H1
-        for well in plate.rows(current_row):
-            current_column = int(well.get_name()[1:])
-            # This is to make sure the column we're putting sybr green into will also have a sample later on
-            if current_column in (sample_range + sybr_only):
+        # Only fill this row if it's found in our samples_range or sybr_only lists
+        if current_row in (sample_range + sybr_only):
+            p100.pick_up_tip(tip)
+            # This loop runs once for every well in the current row of our destination plate, going from A1...H1
+            for well in plate.rows(current_row):
+                # This is to make sure the column we're putting sybr green into will also have a sample later on
                 p100.move_to(sybr_pos)
-                # We start out by priming the tip with our Sybr Green solution
+                # Fill the tip to the maximum volume
                 empty_volume = 100 - p100.current_volume
+                # We start out by priming the tip with our Sybr Green solution
                 p100.mix(2, empty_volume)
                 # This ensures that the tip is always at capacity.
                 p100.aspirate(empty_volume, sybr_pos)
                 # .bottom(3) specifies that dispensing should occur 3mm from the bottom of the well
                 p100.dispense(15, well.bottom(3))
 
+            p100.dispense(p100.current_volume, sybr_pos)
+            p100.blow_out()
+            p100.drop_tip()
         current_row += 1
-        p100.dispense(p100.current_volume, sybr_pos)
-        p100.blow_out()
-        p100.drop_tip()
 
     # Use robot.pause() if using the API through a custom environment (i.e. Jupyter)
     # robot.pause()
     # Sybr green should be refrigerated and samples set on C1
     p100.delay(minutes=15)
-    robot.clear_commands()
+    # robot.clear_commands() # clear_commands for isolating the later parts of a run
 
     # Step 2. Load our sample
 
-    # Here we iterate by row (i.e.: A1...H1) and transfer 5uL x8 of sample to our destination plate w/ Sybr green
-    for tip_row, sample_row, dest_row in zip(t10.rows(sample_range), samples.rows(sample_range), plate.rows(sample_range)):
+    # Here we iterate by row (i.e.: A1...H1) and transfer 5uL x6 of sample to our destination plate w/ Sybr green
+    for tip_row, sample_row, dest_row in zip(t10.rows(sample_range), samples.rows(sample_range),
+                                             plate.rows(sample_range)):
         p50.pick_up_tip(tip_row)
         p50.aspirate(5, sample_row)
         p50.dispense(dest_row)
@@ -86,5 +88,5 @@ def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11)), sybr_
 
 if __name__ == '__main__':
     # Use tip_start_col to define which column should be used from the 10uL tips
-    main(tip_start_col='B', sample_range=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], sybr_only=[0, 11])
+    main(tip_start_col='B', mix_loc='A1', sample_range=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], sybr_only=[0, 11])
     print_history()
