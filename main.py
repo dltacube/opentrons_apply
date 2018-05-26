d62a81cba053207c6c883a03a33a29edb6840c18
[heyoni] <jrevah@gmail.com>
2018-05-26 04:25:29 -0400

    error in prior commit regarding filtering. fixed now

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def print_history():
     for c in robot.commands():
         print(c)
 
-def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11))):
+def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11)), sybr_only=[0, 11]):
     # Step 1. Load the mastermix
 
     # Which column should the robot take tips from?
@@ -50,8 +50,9 @@ def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11))):
         p100.pick_up_tip(tip)
         # This loop runs once for every well in the current row of our destination plate, going from A1...H1
         for well in plate.rows(current_row):
-            # check to be sure
-            if current_row in sample_range:
+            current_column = int(well.get_name()[1:])
+            # This is to make sure the column we're putting sybr green into will also have a sample later on
+            if current_column in (sample_range + sybr_only):
                 p100.move_to(sybr_pos)
                 # We start out by priming the tip with our Sybr Green solution
                 empty_volume = 100 - p100.current_volume
@@ -65,24 +66,25 @@ def main(tip_start_col='A', mix_loc='A1', sample_range=list(range(1, 11))):
         p100.dispense(p100.current_volume, sybr_pos)
         p100.blow_out()
         p100.drop_tip()
-    print_history()
+
     # Use robot.pause() if using the API through a custom environment (i.e. Jupyter)
     # robot.pause()
     # Sybr green should be refrigerated and samples set on C1
     p100.delay(minutes=15)
+    robot.clear_commands()
 
     # Step 2. Load our sample
 
     # Here we iterate by row (i.e.: A1...H1) and transfer 5uL x8 of sample to our destination plate w/ Sybr green
-    for tip_row, sample_row, dest_row in zip(t10.rows, samples.rows(sample_range), plate.rows(1, to=10)):
+    for tip_row, sample_row, dest_row in zip(t10.rows(sample_range), samples.rows(sample_range), plate.rows(sample_range)):
         p50.pick_up_tip(tip_row)
-        p50.aspirate(5, samples.rows(sample_range))
-        p50.dispense(plate.rows(sample_range))
+        p50.aspirate(5, sample_row)
+        p50.dispense(dest_row)
         # Drops the tip in the trash since the p50 is associated with it
         p50.drop_tip()
 
 
-
 if __name__ == '__main__':
     # Use tip_start_col to define which column should be used from the 10uL tips
-    main(tip_start_col='B', sample_range=[0, 2, 3, 4, 6, 7, 8, 9, 10, 11])
+    main(tip_start_col='B', sample_range=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], sybr_only=[0, 11])
+    print_history()
